6924fff7cdcda1b03fdc48cd82dbebd5bda38144
johnpauldecene <162271697+johnpauldecene@users.noreply.github.com>
2024-03-06 13:27:52 +0800

    Add files via upload

---
+++ PAUL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.paul;
+
+/**
+ *
+ * @author STUDENT
+ */
+public class PAUL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
